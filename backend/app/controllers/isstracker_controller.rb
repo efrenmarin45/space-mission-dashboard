89741a20fe5d810d95b0253f776dc5bd207956e6
Efren Marin <efren45marin@gmail.com>
2025-08-17 21:37:11 -0700

    Remove submodule reference and add backend files

---
+++ backend/app/controllers/isstracker_controller.rb
@@ -0,0 +1,13 @@
+class IsstrackerController < ApplicationController
+  include HTTParty
+  BASE_URL = "http://api.open-notify.org/iss-now.json"
+
+  def index
+    response = HTTParty.get(BASE_URL) # HTTParty with correct capitalization
+    if response.success?
+      render json: response.parsed_response
+    else
+      render json: { error: "Failed to fetch current ISS location" }, status: :bad_request
+    end
+  end
+end
